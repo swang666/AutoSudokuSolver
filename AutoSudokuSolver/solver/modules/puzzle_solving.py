13bbf9a0be4c9370395dcf8ecdb66c01b2502791
Sumeng Wang <swang666@g.ucla.edu>
2020-10-20 19:05:24 -0700

    first commit

---
+++ AutoSudokuSolver/solver/modules/puzzle_solving.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import random
+import gc
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array, load_img
+from keras.models import load_model
+from .algorithm import solve
+import datetime
+import os
+
+def read_and_solve(puzzle):
+    base = os.path.dirname(os.path.realpath(__file__))
+    model_dir = os.path.join(base, 'models/digit_rec_model2.h5')
+    model = load_model(model_dir)
+    X = []
+    for i in range(0,9):
+        for j in range(0,9):       
+            crop_img = puzzle[i * 40: (i+1)*40 - 1, j * 40: (j+1)*40 - 1]
+            crop_img = cv2.resize(crop_img, (150,150), interpolation = cv2.INTER_CUBIC)
+            crop_img =  np.expand_dims(crop_img, axis=-1)
+            crop_img =  np.expand_dims(crop_img, axis=0)
+            X.append(crop_img)
+
+    x = np.array(X)
+    
+
+    board = []
+    for i in range(0,9):
+        y = [0] * 9
+        for j in range(0,9):
+            y[j] = model.predict_classes(x[i*9+j])[0]
+        board.append(y)
+
+    solution = solve(board)
+    return solution
---
+++ AutoSudokuSolver/solver/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from . import forms
+import cv2 
+from PIL import Image
+import base64
+from solver.modules.puzzlescan import puzzle_process
+from solver.modules.puzzle_solving import read_and_solve
+
+# Create your views here.
+def index(request):
+    form = forms.ImageForm()
+    payload = {}
+    if request.method == 'POST':
+        form = forms.ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            upFile = request.FILES['img']
+            data = upFile.read()
+            
+            encoded = base64.b64encode(data)
+            processed_img = puzzle_process(encoded)
+            sol = read_and_solve(processed_img)
+            payload['sol'] = sol
+
+            
+    payload['form'] = form     
+
+    return render(request, 'solver/index.html', context = payload)
