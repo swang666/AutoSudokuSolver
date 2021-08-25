60e81bfd2f5c0ac57157114db6308e6557ff9a7e
Sumeng Wang <swang666@g.ucla.edu>
2021-08-24 19:22:21 -0700

    finish dragging points

--- AutoSudokuSolver/solver/urls.py
+++ AutoSudokuSolver/solver/urls.py
@@ -3,4 +3,5 @@ from solver import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('read-image/', views.read_image, name = 'read_image')
 ]
--- AutoSudokuSolver/solver/views.py
+++ AutoSudokuSolver/solver/views.py
@@ -11,6 +11,7 @@ def index(request):
     form = forms.ImageForm()
     payload = {}
     if request.method == 'POST':
+
         form = forms.ImageForm(request.POST, request.FILES)
         if form.is_valid():
             upFile = request.FILES['img']
@@ -20,6 +21,28 @@ def index(request):
             processed_img = puzzle_process(encoded)
             sol = read_and_solve(processed_img)
             payload['sol'] = sol
+
+            
+    payload['form'] = form     
+
+    return render(request, 'solver/index.html', context = payload)
+
+def read_image(request):
+    form = forms.ImageForm()
+    payload = {}
+    if request.method == 'POST':
+
+        form = forms.ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            upFile = request.FILES['img']
+            data = upFile.read()
+            
+            encoded = base64.b64encode(data)
+            decoded = encoded.decode('utf8')
+            #processed_img = puzzle_process(encoded)
+            #sol = read_and_solve(processed_img)
+            #payload['sol'] = sol
+            payload['imgb64'] = decoded
 
             
     payload['form'] = form     
